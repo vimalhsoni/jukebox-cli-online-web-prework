782051e3c040828b0de322db1c8e1bd3505ec7d4
Vimal Soni <vimal.h.soni@gmail.com>
2019-03-09 22:11:22 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -42,24 +42,24 @@ def exit_jukebox
 end
 
 def run(songs)
-command = ""
-  while command
-  puts "Please enter a command:"
-  command = gets.chomp
-  case command
-    when 'list'
-      list(songs)
-      when 'play'
-        list(songs)
-        play(songs)
-      when 'help'
-        help
-      when 'exit'
-        exit_jukebox
-        break
-      else
-        help
-      end
-    end
-  end
-  run(songs)
+# command = ""
+#   while command
+#   puts "Please enter a command:"
+#   command = gets.chomp
+#   case command
+#     when 'list'
+#       list(songs)
+#       when 'play'
+#         list(songs)
+#         play(songs)
+#       when 'help'
+#         help
+#       when 'exit'
+#         exit_jukebox
+#         break
+#       else
+#         help
+#       end
+#     end
+#   end
+#   run(songs)
